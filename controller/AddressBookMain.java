a76500ae725f6959207dcf885ef7b18d7b624498
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-27 12:17:56 +0530

    Added : Display of Welcome Message is Added

---
+++ controller/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.capgemini.assignments.controller;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+			// Display Welcome Message
+			System.out.println("Welcome to Address Book Program");
+	}
+
+}
